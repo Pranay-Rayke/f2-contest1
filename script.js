5e200f0c6c2833daf13ced1a9588fe0bc2376ad5
Pranay Rayke <pranayrayke@gmail.com>
2023-08-12 15:51:08 +0530

    f2-contest1

--- script.js
+++ script.js
@@ -9,18 +9,34 @@ let arr = [
 
 function PrintDeveloper() {
   //Write your code here , just console.log
+  let developer = arr.filter(item=>item.profession=="developer");
+  developer.map(developer=>console.log(developer));
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newEmployee = {id:4, name:'susan', age:20, profession:'intern'};
+    arr.push(newEmployee);
+    console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((item)=> item.profession != "admin")
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newEmployees = [
+    { id: 4, name: 'Pranay', age: 18, profession: 'Designer' },
+    { id: 5, name: 'Sanket', age: 20, profession: 'CEO'},
+    { id: 6, name: 'Sumit', age: 22, profession: 'Product Manager' },
+  ];
+
+  let concatenatedArray = arr.concat(newEmployees);
+  arr = concatenatedArray;
+  console.log(arr);
 }
 
 // Here is an example of how functions work,
